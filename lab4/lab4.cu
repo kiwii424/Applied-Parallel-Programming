ac9f96f331f6a59f44146828e7259d986d1ad514
Meredith <mh126@illinois.edu>
2025-09-26 14:06:47 -0500

    lab4

--- lab4/lab4.cu
+++ lab4/lab4.cu
@@ -11,12 +11,41 @@
   } while (0)
 
 //@@ Define any useful program-wide constants here
+#define MASK_WIDTH 3
 
 //@@ Define constant memory for device kernel here
+__constant__ float deviceKernel[MASK_WIDTH * MASK_WIDTH * MASK_WIDTH];
 
 __global__ void conv3d(float *input, float *output, const int z_size,
                        const int y_size, const int x_size) {
-  //@@ Insert kernel code here
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
+  int z = blockIdx.z * blockDim.z + threadIdx.z;
+
+  if (x >= x_size || y >= y_size || z >= z_size) return;
+
+  int outputIndex = z * y_size * x_size + y * x_size + x;
+  output[outputIndex] = 0.0f;
+
+  int halfWidth = MASK_WIDTH / 2;
+
+  for (int i = -halfWidth; i <= halfWidth; ++i) {
+    for (int j = -halfWidth; j <= halfWidth; ++j) {
+      for (int k = -halfWidth; k <= halfWidth; ++k) {
+        int zz = z + i;
+        int yy = y + j;
+        int xx = x + k;
+
+        if (zz >= 0 && zz < z_size && yy >= 0 && yy < y_size && xx >= 0 && xx < x_size) {
+          int inputIndex = zz * y_size * x_size + yy * x_size + xx;
+          int kernelIndex = (i + halfWidth) * MASK_WIDTH * MASK_WIDTH + 
+                            (j + halfWidth) * MASK_WIDTH + 
+                            (k + halfWidth);
+          output[outputIndex] += input[inputIndex] * deviceKernel[kernelIndex];
+        }
+      }
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -29,6 +58,8 @@ int main(int argc, char *argv[]) {
   float *hostKernel;
   float *hostOutput;
   //@@ Initial deviceInput and deviceOutput here.
+  float *deviceInput;
+  float *deviceOutput;
 
   args = wbArg_read(argc, argv);
 
@@ -50,6 +81,8 @@ int main(int argc, char *argv[]) {
   //@@ Allocate GPU memory here
   // Recall that inputLength is 3 elements longer than the input data
   // because the first  three elements were the dimensions
+  wbCheck(cudaMalloc((void **)&deviceInput, (inputLength - 3) * sizeof(float)));
+  wbCheck(cudaMalloc((void **)&deviceOutput, (inputLength - 3) * sizeof(float)));
 
 
 
@@ -57,12 +90,18 @@ int main(int argc, char *argv[]) {
   // Recall that the first three elements of hostInput are dimensions and
   // do
   // not need to be copied to the gpu
+  wbCheck(cudaMemcpy(deviceInput, &hostInput[3], (inputLength - 3) * sizeof(float),
+                     cudaMemcpyHostToDevice));
+  wbCheck(cudaMemcpyToSymbol(deviceKernel, hostKernel, MASK_WIDTH * MASK_WIDTH * MASK_WIDTH * sizeof(float)));
 
 
 
   //@@ Initialize grid and block dimensions here
+  dim3 DimGrid((x_size + 7) / 8, (y_size + 7) / 8, (z_size + 7) / 8);
+  dim3 DimBlock(8, 8, 8);
 
   //@@ Launch the GPU kernel here
+  conv3d<<<DimGrid, DimBlock>>>(deviceInput, deviceOutput, z_size, y_size, x_size);
 
   cudaDeviceSynchronize();
 
@@ -71,7 +110,8 @@ int main(int argc, char *argv[]) {
   //@@ Copy the device memory back to the host here
   // Recall that the first three elements of the output are the dimensions
   // and should not be set here (they are set below)
-
+  wbCheck(cudaMemcpy(&hostOutput[3], deviceOutput, (inputLength - 3) * sizeof(float),
+                     cudaMemcpyDeviceToHost));
 
 
 
@@ -82,11 +122,13 @@ int main(int argc, char *argv[]) {
   wbSolution(args, hostOutput, inputLength);
 
   //@@ Free device memory
-
+  wbCheck(cudaFree(deviceInput));
+  wbCheck(cudaFree(deviceOutput));
 
   // Free host memory
   free(hostInput);
   free(hostOutput);
+  free(hostKernel);
   return 0;
 }
 
